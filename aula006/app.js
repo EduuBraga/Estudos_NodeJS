a55f8de19d2f2e1db521094b3b5cf6341da5e2fd
Eduardo Braga <chocolate.blindado123@gmail.com>
2022-12-19 23:12:35 -0300

    Comprimindo imagens com "compress_images"

--- aula006/app.js
+++ aula006/app.js
@@ -1,18 +1,48 @@
 const sharp = require('sharp')
+const compress_images = require('compress-images')
+const fs = require('fs')
 
+console.log(compress_images)
 const imgFile = process.argv[2]
 const width = Number(process.argv[3])
 
-const resizeImg = (img, width) => {
-  sharp(img)
-    .resize({width: width})
-    .toFile('./temp/imgTest1.jpg', (error) => {
-      if(error) {
-        console.log(error.message)
+const resizeImg = (pathInput, pathOutput, width) => {
+  sharp(pathInput)
+    .resize({ width: width })
+    .toFile(pathOutput, error => {
+      if (error) {
+        console.log(error)
+        return
       }
 
       console.log('Imagem redimensionada com sucesso!')
+      compress(pathOutput, '/compress/')
     })
 }
 
-resizeImg(imgFile, width)
+const compress = (pathInput, pathOutput) => {
+  compress_images(pathInput, pathOutput, 
+    { compress_force: false, statistic: true, autoupdate: true }, false,
+    { jpg: { engine: "mozjpeg", command: ["-quality", "60"] } },
+    { png: { engine: "pngquant", command: ["--quality=20-50", "-o"] } },
+    { svg: { engine: "svgo", command: "--multipass" } },
+    { gif: { engine: "gifsicle", command: ["--colors", "64", "--use-col=web"] } },
+    (error, completed, statistic) => {
+      console.log("-------------");
+      console.log(error);
+      console.log(completed);
+      console.log(statistic);
+      console.log("-------------");
+
+      fs.unlink(pathInput, error => {
+        if(error){
+          console.log(error)
+        }else{
+          console.log(`${pathInput} excluído com sucesso!`)
+        }
+      })
+    }
+  );
+}
+
+resizeImg(imgFile, './temp/imgTest1.jpg', width)
