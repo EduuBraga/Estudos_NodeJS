35690856906520932cc0b2644eda04d6d1e5ce9d
Eduardo Braga <chocolate.blindado123@gmail.com>
2022-12-20 15:33:34 -0300

    Tratando arquivos desconhecidos

--- aula008/index.js
+++ aula008/index.js
@@ -2,29 +2,56 @@ const HTTP = require('http');
 const URL = require('url');
 const FS = require('fs');
 
-HTTP.createServer((req, res) => {
+const handleFile = (req, res, callback) => {
   let path = URL.parse(req.url).pathname;
-  if(path === '' || path === '/'){
-    path = '/index.html';
+  let pathIsEmpty = ['', '/'].includes(path)
+
+  if (pathIsEmpty) { path = '/index.html'; }
+
+  let fileName = `.${path}`;
+
+  callback(fileName, res, path)
+}
+
+const handleContentResponse = (res, content) => {
+    res.writeHead(200, { 'Content-type': 'text/plain;charset=utf-8' })
+    res.end(content)
+}
+
+const handleRequest = (path, res) => {
+  switch (path) {
+    case '/test1':
+      handleContentResponse(res, 'Página de teste 1.')
+      return true
+    case '/test2':
+      handleContentResponse(res, 'Página de teste 2.')
+      return true
+    default:
+      return false
   }
+}
 
-  const fileName = `.${path}`;
-
+const handleResponse = (fileName, res, path) => {
   FS.readFile(fileName, (error, data) => {
-    if(error){
-      res.writeHead(404, {'Content-type': 'text/html;charset=utf-8'});
+    if (error) {
+      if (handleRequest(path, res)) { return null }
+
+      res.writeHead(404, { 'Content-type': 'text/html;charset=utf-8' });
       res.end("<h1>Página não encontrada</h1>");
-    }else{
-      res.writeHead(200, {'Content-type': 'text/html'});
+    } else {
+      res.writeHead(200, { 'Content-type': 'text/html' });
       res.write(data);
       res.end();
     }
   })
+}
 
+HTTP.createServer((req, res) => {
+  handleFile(req, res, handleResponse)
 }).listen(3000, error => {
-  if(error){
+  if (error) {
     console.log(error);
-  }else{
+  } else {
     console.log("Servidor rodando na porta 3000");
   }
 });
