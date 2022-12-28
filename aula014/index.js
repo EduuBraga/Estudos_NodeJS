413be8c39995426433a9539c69cf9ec7315c8aeb
Eduardo Braga <chocolate.blindado123@gmail.com>
2022-12-28 16:06:01 -0300

    Utilizando o FOR com o EJS

--- aula014/index.js
+++ aula014/index.js
@@ -1,21 +1,30 @@
 const express = require('express')
-const FS = require('fs')
 const path = require('path')
 
 const server = express()
 const PORT = 3000
 
-const user = {
-  name: 'Eduardo braga',
-  username: 'EduuBraga',
-  email: 'eduardo.braga467@gmail.com'
-}
+const users = [
+  {
+    name: 'Eduardo braga',
+    username: 'EduuBraga',
+    email: 'eduardo.braga467@gmail.com'
+  }, {
+    name: 'Yasmim binbin',
+    username: 'YasmimBinBin',
+    email: 'Yasmim.binbin@gmail.com'
+  }, {
+    name: 'Pablo vasconcelhos',
+    username: 'Palo123',
+    email: 'Pablo.vasconcelhos@gmail.com'
+  }
+]
 
 server.set('views', path.join(__dirname, 'views'))
 server.set('view engine', 'ejs')
 
 server.get('/', (req, res) => {
-  res.render('user', user)
+  res.render('user', { users })
 })
 
 server.listen(PORT, () => console.log("Server running on PORT", 3000))
