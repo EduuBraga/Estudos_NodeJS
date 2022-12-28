ed0836fcb4c446c4f4b180cf6ffd98674ee74bb1
Eduardo Braga <chocolate.blindado123@gmail.com>
2022-12-28 15:45:51 -0300

    Manipulando variáveis com EJS

--- aula014/index.js
+++ aula014/index.js
@@ -5,10 +5,17 @@ const path = require('path')
 const server = express()
 const PORT = 3000
 
+const user = {
+  name: 'Eduardo braga',
+  username: 'EduuBraga',
+  email: 'eduardo.braga467@gmail.com'
+}
+
 server.set('views', path.join(__dirname, 'views'))
+server.set('view engine', 'ejs')
 
 server.get('/', (req, res) => {
-
+  res.render('user', user)
 })
 
 server.listen(PORT, () => console.log("Server running on PORT", 3000))
