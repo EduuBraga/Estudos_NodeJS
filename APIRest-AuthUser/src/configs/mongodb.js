5decbd0fd9eefcef0e86a47c743d7e4e142db5e1
Eduardo Braga <chocolate.blindado123@gmail.com>
2023-01-09 16:50:58 -0300

    Feat: Configurei a conexão com o DB

---
+++ APIRest-AuthUser/src/configs/mongodb.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const URL = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@apiauthuser.ibgxfs0.mongodb.net/?retryWrites=true&w=majority`;
+
+const startDB = () => {
+  mongoose.set('strictQuery', false);
+  mongoose.connect(URL)
+    .then(_ => console.log('Banco carregado'))
+    .catch(err => console.log(err));
+};
+
+module.exports = startDB;
--- APIRest-AuthUser/src/index.js
+++ APIRest-AuthUser/src/index.js
@@ -1,5 +1,8 @@
 require('dotenv').config();
 const app = require('./app');
+const startDB = require('./configs/mongodb');
 const PORT = 3000;
+
+startDB();
 
 app.listen(process.env.PORT || PORT, () => console.log('Server running'));
