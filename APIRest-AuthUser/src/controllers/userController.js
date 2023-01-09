bd4f2cb6e05598134399359cb7bb76b006bc9d6d
Eduardo Braga <chocolate.blindado123@gmail.com>
2023-01-09 17:12:45 -0300

    Feat: configurando o controller de registrar user

--- APIRest-AuthUser/src/controllers/userController.js
+++ APIRest-AuthUser/src/controllers/userController.js
@@ -1,9 +1,19 @@
+const userModel = require('../models/userModel')
+
 const userController = {
   login: (req, res) => {
-    res.send('Login');
+    res.send('login')
   },
-  register: (req, res) => {
-    res.send('register');
+  register: async (req, res) => {
+    const { name, password, email } = req.body;
+    const newUser = new userModel({ name, password, email });
+
+    try {
+      const savedUser = await newUser.save();
+      res.status(200).send(savedUser);
+    } catch (error) {
+      res.status(400).send(error.message);
+    }
   }
 };
 
